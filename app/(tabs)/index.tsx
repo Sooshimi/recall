7e23082c1f74900f788afcbe8e58eab1e340fee2
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2026-02-19 14:55:28 +0000

    Add simple card component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native'
+import {FlatList, StyleSheet, Text, View} from 'react-native'
 import React from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
+import Card from '@/components/Card';
 
 const Index = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Home page</Text>
+      <Card />
     </SafeAreaView>
   )
 }
---
+++ components/Card.tsx
@@ -0,0 +1,34 @@
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native'
+import React from 'react';
+
+const Card = () => {
+	return (
+		<View style={styles.container}>
+			<TextInput style={styles.textBox} placeholder={"Type here"} placeholderTextColor={"gray/"}></TextInput>
+			<Button
+				title="+"
+				onPress={() => alert("Button clicked")}
+				color={"white"}
+			>
+			</Button>
+		</View>
+	)
+}
+
+export default Card
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'stretch',
+		backgroundColor: '#43607a',
+		paddingTop: 20,
+		paddingBottom: 20,
+		paddingLeft: 20,
+		paddingRight: 10
+	},
+	textBox: {
+		flex: 1,
+		backgroundColor: '#fff'
+	}
+})
