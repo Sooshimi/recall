a59aec3dd67effde5d616cbd40685dc7fd4ab6fd
Sooshimi <59628017+Sooshimi@users.noreply.github.com>
2026-02-24 14:59:13 +0000

    change recall card when marked as read

--- app/(tabs)/recall.tsx
+++ app/(tabs)/recall.tsx
@@ -1,5 +1,5 @@
 import {Button, StyleSheet, View} from 'react-native'
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {SafeAreaView} from "react-native-safe-area-context";
 
 import Card from "@/components/Card";
@@ -7,15 +7,36 @@ import {CardsContext} from "@/context/CardsContext";
 
 const Recall = () => {
 	// @ts-ignore
-	const { cards, readPressed } = useContext(CardsContext)
+	const { cards, readPressed } = useContext(CardsContext);
+	const [ currentCard, setCurrentCard ] = useState<number>(0);
+
+	const readPressedHandler = () => {
+		if (!cards || cards.length === 0) return;
+
+		readPressed(cards[currentCard].word);
+
+		// Loop back to first card when reaching end of card list
+		if (currentCard < cards.length - 1) {
+			setCurrentCard(currentCard + 1);
+		} else {
+			setCurrentCard(0);
+		}
+	};
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<View style={styles.cardContainer}>
 				{ cards && cards.length > 0 && (
-					<Card text={cards[0].word} definition={cards[0].definition} readCount={cards[0].readCount} />
+					<Card
+						text={cards[currentCard].word}
+						definition={cards[currentCard].definition}
+						readCount={cards[currentCard].readCount} />
 				)}
-				<Button title="Mark as read" color='gray' onPress={() => readPressed(cards[0].word)}></Button>
+				<Button
+					title="Mark as read"
+					color='gray'
+					onPress={() => {readPressedHandler()}}>
+				</Button>
 			</View>
 		</SafeAreaView>
 	)
